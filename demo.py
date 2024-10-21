796143bf772a2fcbac1f92914032c480478506c2
Baudet-Antoine <168273111+Baudet-Antoine@users.noreply.github.com>
2024-10-21 16:59:07 +0200

    I do not care! Message

---
+++ demo.py
@@ -0,0 +1,5 @@
+from src.maincode.custom_functions import is_even
+
+if __name__ == '__main__':
+    N = 12345
+    print(is_even(N))
---
+++ src/maincode/custom_functions.py
@@ -0,0 +1,5 @@
+def is_odd(n : int) -> bool:
+    return n % 2
+
+def is_even(n : int) -> bool:
+    return not is_odd(n) 
