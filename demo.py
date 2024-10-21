c73a51679e90ad96849a38beb7ad75f82420c407
Baudet-Antoine <168273111+Baudet-Antoine@users.noreply.github.com>
2024-10-21 17:15:49 +0200

    Demo test

--- demo.py
+++ demo.py
@@ -1,5 +1,20 @@
 from src.maincode.custom_functions import is_even
 
+import streamlit as st
+
+from simplepypkg.custom_functions import is_even, is_odd
+
+
+def app():
+    st.header('Test if your number is Even 💚')
+    n = st.slider('Provide an integer to test', min_value = 0, max_value = 100)
+    if n:
+        resp = is_even(n)
+        if resp:
+            st.success(f'{n} is even !')
+        else:
+            st.info(f'{n} is odd...')
+
+
 if __name__ == '__main__':
-    N = 12345
-    print(is_even(N))
+	app()
